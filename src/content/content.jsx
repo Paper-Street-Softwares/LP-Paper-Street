cb56a9a766cc75e6131e0a1e5091bf5b7e51ac94
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-03-05 17:03:41 -0300

    fix: renomeia imagens

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -15,12 +15,12 @@ import imgSteps from "../assets/imgs/steps/imgSteps.webp";
 // import logo2 from '../assets/imgs/parceria/Logo2.webp'
 // import logo3 from '../assets/imgs/parceria/Logo3.webp'
 
-import feed1 from "../assets/imgs/socialProof/feed1.webp";
-import feed2 from "../assets/imgs/socialproof/feed2.webp";
-import feed3 from "../assets/imgs/socialproof/feed3.webp";
-import feed4 from "../assets/imgs/socialproof/feed4.webp";
-import feed5 from "../assets/imgs/socialproof/feed5.webp";
-import feed6 from "../assets/imgs/socialproof/feed6.webp";
+import feed1 from "../assets/imgs/socialProof/social1.webp";
+import feed2 from "../assets/imgs/socialproof/social2.webp";
+import feed3 from "../assets/imgs/socialproof/social3.webp";
+import feed4 from "../assets/imgs/socialproof/social4.webp";
+import feed5 from "../assets/imgs/socialproof/social5.webp";
+import feed6 from "../assets/imgs/socialproof/social6.webp";
 
 import {
   Baby,
